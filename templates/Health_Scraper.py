3599d6f4d0dcad2eafb237558a07ce1b8845b817
James Cheung <jamescheung@Jamess-MacBook-Pro-3.local>
2019-02-06 23:43:25 -0500

    asidc

--- templates/Health_Scraper.py
+++ templates/Health_Scraper.py
@@ -3,6 +3,66 @@ from selenium import webdriver
 driver = webdriver.Chrome('/Users/jamescheung/chromedriver')
 import datetime
 now = datetime.datetime.now()
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+import re
+from nltk.stem import WordNetLemmatizer
+import heapq  
+from textblob import TextBlob
+
+def Summarizer(article_text):
+    # Removing Square Brackets and Extra Spaces
+    article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)  
+    article_text = re.sub(r'\s+', ' ', article_text)
+
+    # Removing special characters and digits
+    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )  
+    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text) 
+    
+    lemma = WordNetLemmatizer()
+    formatted_article_text = lemma.lemmatize(article_text)
+    
+    clean_list = []
+    sentence_list = TextBlob(formatted_article_text).sentences
+    for sent in sentence_list:
+        clean_list.append(str(sent))
+        
+    stopwords = nltk.corpus.stopwords.words('english')
+
+    word_frequencies = {}  
+    for word in word_tokenize(formatted_article_text):
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word.lower()] = 1
+            else:
+                word_frequencies[word.lower()] += 1
+
+    maximum_frequency = max(word_frequencies.values())
+    max_keys = [k for k, v in word_frequencies.items() if v == maximum_frequency]
+
+    for word in word_frequencies.keys():  
+        word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
+
+    sentence_scores = {}  
+    for sent in clean_list:  
+        for word in TextBlob(formatted_article_text).words:
+            if word in word_frequencies.keys():
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word]
+                else:
+                    sentence_scores[sent] += word_frequencies[word]
+    
+    summary_sentences = heapq.nlargest(5, sentence_scores, key=sentence_scores.get)
+    if len(' '.join(summary_sentences)) > 1000:
+        summary_sentences = heapq.nlargest(4, sentence_scores, key=sentence_scores.get)
+    max_keys = heapq.nlargest(10, word_frequencies, key=word_frequencies.get)
+    for x in max_keys:
+        if x.isalpha():
+            Max = x
+            break
+    summary = ' '.join(summary_sentences)
+    return(summary,Max) 
 
 
 def Scrape_Health_Affairs():
@@ -180,6 +240,25 @@ def Scrape_Fox():
         Spliced_Title = Title[0:5]
     return(Spliced_Title, Spliced_Link,url)
 
+def Get_Text_Fox(Link_List):
+    Link_Fox = Link_List[1]
+    Articles = []
+    Strong = []
+    for article in Link_Fox:
+        driver.get(article)
+        driver.implicitly_wait(30)
+        html1 = driver.page_source
+        soup1 = BeautifulSoup(html1, 'lxml')
+        article_text = ""
+        all_ps = soup1.select('div[class=article-body] p')
+        for p in all_ps[1:]:
+            if p.get_text().isupper():
+                Strong.append(p)
+            else:
+                article_text += p.get_text() + ' '
+        Articles.append(article_text)
+    return(Articles)
+
 def Scrape_MSNBC():
     url ='https://www.nbcnews.com/'
     driver.get(url)
@@ -238,6 +317,126 @@ def HTML_Writer(Text,Name):
         <head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <style>
+
+* {
+    margin: 0;
+}
+
+@media only screen and (min-width:601px){
+  .all {
+    padding-left: 35%;
+    padding-right: 35%;
+    
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .all {
+    padding-left: 5%;
+    padding-right: 5%;
+
+  }
+}
+
+body {
+  margin-bottom:20%;
+  padding-top: 5%;
+}
+
+h1 {
+  text-align: center;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+  position: fixed;
+  width: 100%;
+  top: 0;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.link:hover {
+  color: blue;
+}
+        </style>
+        <title>HEALTH SUMMARIES</title>
+        </head>
+        <body>
+        <div class = 'topnav'>
+        <a class='active' href='/'> Front Page</a>
+        <a href="/news">News</a>
+        <a href="/health">Health</a>
+        <a href="/pharma">Pharma</a>
+        </div>
+        <div class='all'>"""
+    name = """<h1><a href='"""+str(url)+"""'>""" +str(Name)+ """</a></h1>"""
+    date = "<h4>"+now.strftime("%Y-%m-%d %H:%M")+"</h4>"
+    tail = """</div></body>
+        </html>"""
+    return(head,name,date,body_html,tail)
+
+def Summary_Writer(Text,Name,Article_List):
+    Title = Text[0]
+    Link = Text[1]
+    url = Text[2]
+    Articles = Article_List
+
+    article_list = []
+    for i in range(len(Articles)):
+        article_list.append('article'+str([i+1]))
+    article_dict = dict(zip(article_list,Articles))
+
+    title_list = []
+    for i in range(len(Title)):
+        title_list.append('title'+str([i+1]))
+    title_dict = dict(zip(title_list, Title))
+    
+    link_list = []
+    for i in range(len(Link)):
+        link_list.append('link'+str([i+1]))
+    link_dict = dict(zip(link_list,Link))
+
+    article_html = []
+    for i in range(len(Articles)):
+        article_html.append("<p>"+str(Summarizer(article_dict[article_list[i]])[0]+"</p>"))
+    
+    title_html = []
+    for i in range(len(Title)):
+        title_html.append("""<h3><a class='link' href='"""+str(link_dict[link_list[i]])+"""'>"""+str(title_dict[title_list[i]]+"</a></h3>"))
+
+    body_html = ""
+    for i in range(len(Title)):
+        body_html += title_html[i] + article_html[i] + "<br>"       
+    head = """
+    <!DOCTYPE html>
+        <html>
+        <head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <style>
+
+* {
+    margin-left: 0;
+}
+
 @media only screen and (min-width:601px){
   .all {
     padding-left: 35%;
@@ -322,6 +521,7 @@ JAMA = Scrape_JAMA()
 Fierce = Scrape_Fierce()
 Biospace = Scrape_Biospace()
 Fox = Scrape_Fox()
+Fox_Articles = Get_Text_Fox(Fox)
 MSNBC = Scrape_MSNBC()
 CNN = Scrape_CNN()
 
@@ -347,7 +547,7 @@ def Pharma_Combinator():
     return(Page)
 
 def News_Combinator():
-    Page_Fox = HTML_Writer(Fox, "Fox News")
+    Page_Fox = Summary_Writer(Fox, "Fox News", Fox_Articles)
     Page_MSNBC = HTML_Writer(MSNBC, 'MSNBC')
     Page_CNN = HTML_Writer(CNN, 'CNN')
     Page = Page_Fox[0]+Page_Fox[2]+Page_Fox[1]+Page_Fox[3]+'<br>'+Page_MSNBC[1]+Page_MSNBC[3]+'<br>'+Page_CNN[1]+Page_CNN[3]+Page_CNN[4]
