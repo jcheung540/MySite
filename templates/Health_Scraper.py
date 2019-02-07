2a0c418bd2ade59f581abeb52c584c726285b20c
James Cheung <jamescheung@Jamess-MacBook-Pro-3.local>
2019-02-06 19:52:12 -0500

    updates

--- templates/Health_Scraper.py
+++ templates/Health_Scraper.py
@@ -237,7 +237,8 @@ def HTML_Writer(Text,Name):
         <html>
         <head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <link rel="stylesheet" href="stylesheet.css>
+        <link rel="stylesheet" href="stylesheet.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <title>HEALTH SUMMARIES</title>
         </head>
         <body>
